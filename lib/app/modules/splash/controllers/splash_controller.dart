2279a9748c6561287637299933421b895c097318
Arya Rezza Anantya <43742778+anantyan@users.noreply.github.com>
2022-09-15 22:24:54 +0700

    Add Splashscreen

---
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+
+import '../../../routes/app_pages.dart';
+
+class SplashController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    redirect();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Get.offAllNamed(Routes.LOGIN_PAGE_VIEW);
+  }
+}
---
+++ lib/app/modules/splash/views/splash_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/splash_controller.dart';
+
+class SplashView extends GetView<SplashController> {
+  const SplashView({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.abc,
+              size: 120.0,
+              color: Colors.blueAccent,
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
